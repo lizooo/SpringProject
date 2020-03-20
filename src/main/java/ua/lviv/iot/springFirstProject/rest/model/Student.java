e08dd0fa7a873e84407bad26dd54b9052bd11887
Liza Dudyk <lizadudyk@gmail.com>
2020-03-20 10:08:56 +0200

    Added code from Spring boot webinars 1,2,3

---
+++ src/main/java/ua/lviv/iot/springFirstProject/rest/model/Student.java
@@ -0,0 +1,43 @@
+package ua.lviv.iot.springFirstProject.rest.model;
+
+public class Student {
+
+  public Student(String firstName, String secondName) {
+    this.firstName = firstName;
+    this.secondName = secondName;
+  }
+
+  public Student() {
+
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getSecondName() {
+    return secondName;
+  }
+
+  public void setSecondName(String secondName) {
+    this.secondName = secondName;
+  }
+
+  private String firstName;
+  private String secondName;
+  private int Id;
+  
+  public int getId() {
+    return Id;
+  }
+
+  public void setId(int id) {
+    this.Id = id;
+  }
+
+ 
+}
